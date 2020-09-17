0d8e0d472fa42b9e39213a5d129d43d9fa504b19
Marc Wright <marc.wright@gmail.com>
2020-09-17 13:01:34 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// Try to manipulate the page
+
+// Easiest
+// 1) Get the title for each dog/cat services, and put them into the console using console.log
+// 2) Change the title of the page to "We're the CAT People"
+// 3) Change the main image to be something cat related.
+
+// Harder
+// 1) Replace the call to action button background color with "red"
+// 2) Find another image and replace the header (try unsplash.com, get familiar with searching for images)
+// 3) When someone clicks the "Book your next Rover sitter" button, change the text of the button to say "Thanks"
+
+// Hardest (only if you get the others first)
+// 1) Make the responsive menu work!
+// 2) Implement a modal window - "Sign up now" CTA when someone clicks the 'Sign Up' button
+
+// const titles = document.querySelectorAll(".rover-facts-container h3");
+// console.log(titles);
+
+// const header = document.querySelector(".rover-header-container");
+// header.innerHTML = "";
+// const newImg = document.createElement("img");
+// newImg.setAttribute(
+//   "src",
+//   "https://images.unsplash.com/photo-1599366614021-07261fe8d9d7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3591&q=80"
+// );
+// const mainContainer = document.querySelector(".rover-hp-container");
+// newImg.style.width = "100vw";
+// newImg.style.height = "400px";
+// mainContainer.insertBefore(newImg, header);
